eeade860558d82748a7d25fbb24adef104889b89
HaianhNguyenHo <nguyenhohaianh2@gmail.com>
2024-12-11 09:12:19 +0700

    a

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Hiệu");
+        System.out.println("Nhập số thứ nhất");
     }
 }
